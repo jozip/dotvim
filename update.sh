d0526e8b696f848e8e1b8f3f61927b2c0ab5715b
Johan Persson <jzp@bahnhof.se>
2018-05-09 15:50:50 +0200

    Fancier update script

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+MESSAGE=$(mktemp -p /tmp dotvimXXXX)
 git submodule update --remote --merge
-git commit
+echo "Updated submodules" > $MESSAGE
+echo >> $MESSAGE
+git status -s | awk '{print $2}' | sed -e 's%pack/stuff/start/%%' >> $MESSAGE
+git commit -a -F $MESSAGE
